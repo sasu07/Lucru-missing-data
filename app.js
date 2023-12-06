cf3e27de19d17e25df1257aabc7ece72745586b7
Sasu Gabriel <77246287+sasu07@users.noreply.github.com>
2023-12-06 11:00:27 +0200

    update

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
 const express = require('express');
-const morgan = require('morgan');
-const tourRouter = require ('./routes/tourRoutes');
-const userRouter = require ('./routes/userRoutes');
 
 const app = express();
 
-
-
-// MIDDLEWARE
-
-
-
-console.log(process.env.NODE_ENV);
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-}
-
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
-
-app.use((req, res, next) => {
-    console.log('Hello from the middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+app.get('/', (req, res) => {
+    res.status(200).json({message:'Salut'}); 
 });
 
 
-//ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+const port = 3000
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
 
 
-//START THE SERVER
 
-module.exports = app;
