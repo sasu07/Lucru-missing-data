21647c48ccfebe7c70df331d421d563591425751
Sasu Gabriel <77246287+sasu07@users.noreply.github.com>
2023-12-06 09:59:07 +0200

    begin

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require ('./routes/tourRoutes');
+const userRouter = require ('./routes/userRoutes');
+
+const app = express();
+
+
+
+// MIDDLEWARE
+
+
+
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
+
+//ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+
+//START THE SERVER
+
+module.exports = app;
