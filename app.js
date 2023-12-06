b9397576835806717fa823a06131ca6da5a30363
Sasu Gabriel <77246287+sasu07@users.noreply.github.com>
2023-12-06 14:56:03 +0200

    update

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).json({message:'Salut'}); 
-});
+const fids = JSON.parse(fs.readFileSync(`${__dirname}/fids.json`));
+
+const getAllFids = (req, res) => {
+    res.status(200).json({
+        status: 'succes',
+        results: fids.length,
+        data: {
+            fids
+        }
+    });
+};
+
+const createFids = (req,res) => {
+    const newId = fids[fids.length - 1].id + 1;
+    const newFids = Object.assign({id: newId }, req.body);
+    console.log(req.body)
+    fids.push(newFids);
+    fs.writeFile( `${__dirname}/fids.json`, JSON.stringify(fids), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                fids: newFids
+            }
+        })
+    })
+};
+
+
+//app.get('/api/v1/fids', getAllFids)
+//app.post('/api/v1/fids', createFids)
+
+
+app
+    .route('/api/v1/fids')
+    .get(getAllFids)
+    .post(createFids);
 
 
 const port = 3000
